544fa44e678a35bf2f4d7331a4c5529aba60fac2
Oladele <oladele@gmail.com>
2015-10-21 17:36:10 -0400

    Prompt user to create song after creating band

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,11 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	noSongs: Ember.computed('model.songs.length', function() {
+		return this.get('model.songs.length') === 0; 
+	}),
+	songCreationStarted: false,
+	canCreateSong: Ember.computed('songCreationStarted', 
+		'model.songs.length', function() {
+		return this.get('songCreationStarted') || this.get('model.songs.length');
+	}),
+	
 	actions: {
 		updateRating: function(params) {
 			var song = params.item,
 			rating = params.rating;
 			song.set('rating', rating);
+		},
+
+		enableSongCreation: function(){
+			this.set('songCreationStarted', true);
 		}
 	}
 });
