1cc30762acbef402fa1b996f05624db90dc228b4
Oladele <oladele@gmail.com>
2015-10-22 10:01:52 -0400

    Get bands and songs from server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,10 @@
 import DS from 'ember-data';
+import ENV from '../config/environment';
 
-export default DS.RESTAdapter.extend({
+export default DS.JSONAPIAdapter.extend({
+	host: ENV.apiHost,
+
+	shouldBackgroundReloadRecord: function(){
+		return false;
+	}
 });
--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(params) {
 		var bands = this.modelFor('bands');
-		return bands.get('content').findBy('slug', params.slug);
+		// return bands.get('content').findBy('slug', params.slug);
 		// params.slug is now 'pearl-jam'
+		return this.store.findRecord('band', params.id);
+		// use ember data to fetch from server
 	}
 });
