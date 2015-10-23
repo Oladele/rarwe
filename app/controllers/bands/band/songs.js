05a3ba848397b1d0c6eb83afb7f171a052a310ec
Oladele <oladele@gmail.com>
2015-10-23 10:46:52 -0400

    Search songs

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -31,7 +31,20 @@ export default Ember.Controller.extend({
 		return options[this.get('sortBy')].split(',');
 	}),
 
-  sortedSongs: Ember.computed.sort('model.songs', 'sortProperties'),
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
+
+  // Search Songs
+  searchTerm: '',
+	matchingSongs: Ember.computed(
+		'model.songs.@each.title', 
+		'searchTerm',
+		function() {
+			var searchTerm = this.get('searchTerm').toLowerCase();
+			return this.get('model.songs').filter(function(song) {
+				return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+			});
+		}
+	),
 	
 	actions: {
 		updateRating: function(params) {
