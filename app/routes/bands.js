4d7d86893d3a3cb4dfb88ed537669921eebadf13
Oladele <oladele@gmail.com>
2015-10-26 08:41:03 -0400

    Extract wait to utility folder.

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,15 +1,5 @@
 import Ember from 'ember';
-
-function wait(promise, delay) {
-	return new Ember.RSVP.Promise(function(resolve) {
-		setTimeout(function() { 
-			promise.then(function(result) {
-	    	resolve(result);
-	    });
-	  }, delay);
-  });
-}
-
+import wait from '../utils/wait';
 
 export default Ember.Route.extend({
 	model: function(){
