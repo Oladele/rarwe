3b77145e3229db9c141d73e73340548c374b3207
Oladele <oladele@gmail.com>
2015-11-02 06:11:33 -0500

    initial bands-dashboard

--- app/routes/bands-dashboard.js
+++ app/routes/bands-dashboard.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function(){
+		return this.store.findAll('band');
+	},
+
+	actions: {
+		didTransition: function() {
+      document.title = 'Bands - Dashboard';
+    }
+	}
 });
